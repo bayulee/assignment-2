3fa424383c2e808a64e400caf0c2a975ddc18f5c
bayulee <bayuleepaul@gmail.com>
2022-03-17 10:06:02 +0000

    first commit

---
+++ src/writers.js
@@ -0,0 +1,39 @@
+const writers =[
+    {
+        id:1,
+        name: "jamila lord",
+        avatar:"audre_lorde",
+        email: "lorde@gmail.com",
+        phone: "0556453553"
+    },
+    {
+        id:2,
+        name: "jude lorde bam",
+        avatar:"belle_hooks",
+        email: "lorde@gmail.com",
+        phone: "0556453553"
+    },
+    {
+        id:3,
+        name: "Ama ghana",
+        avatar:"du_bois",
+        email: "lorde@gmail.com",
+        phone: "0556453553"
+    },
+    {
+        id:4,
+        name: "felicia kam lord",
+        avatar:"james_baldwin",
+        email: "lorde@gmail.com",
+        phone: "0556453553"
+    },
+    {
+        id:5,
+        name: "aya gyan lord",
+        avatar:"maya_angelou",
+        email: "gyan@gmail.com",
+        phone: "0556453553"
+    }
+]
+
+export default writers;
